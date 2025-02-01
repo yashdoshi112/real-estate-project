6f0a62d14d1e240647b94e2387c932081857df1c
yashdoshi112 <yashdoshi112@gmail.com>
2025-02-01 20:04:26 +0530

    change

--- backend/config/db.js
+++ backend/config/db.js
@@ -8,6 +8,7 @@ const pool = new Pool({
   database: 'postgres',
   password: 'yash_doshi01',
   port: 5432,
+  connectionString: process.env.DATABASE_URL || 'postgres://u1okb793060lns:p25359bd2132785017495406133f27957e7d53de2ed2315af26bc89e617bfbf5c@c5p86clmevrg5s.cluster-czrs8kj4isg7.us-east-1.rds.amazonaws.com:5432/dcd5nu1p2efp4o'
 });
 
 module.exports = pool;
